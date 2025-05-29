0c3b3c7d6f18716d8242cd479f411f8b319567fb
Saloni Kahar <salonikahar20@gmail.com>
2025-05-29 23:53:53 +0530

    ejs

--- Movie-app/app.js
+++ Movie-app/app.js
@@ -1,2 +1,7 @@
 let express =  require('express')
-let port = 8001
+let port = 8001;
+let app =  express()
+
+app.listen(port ,function(err){
+    err ? console.log(err) : console.log('server is running on port:',port);
+})
