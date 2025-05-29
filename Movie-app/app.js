c2605942abebefa3b83c8d91c8ca0ad6d83a6d91
Saloni Kahar <salonikahar20@gmail.com>
2025-05-30 00:53:09 +0530

    form created

--- Movie-app/app.js
+++ Movie-app/app.js
@@ -1,6 +1,18 @@
 let express =  require('express')
-let port = 8001;
+let port = 8010;
 let app =  express()
+let path = require('path');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname , 'views'));
+
+app.get('/', (req , res)=>{
+    return res.render('index');
+})
+
+app.get('/add', (req , res)=>{
+    return res.render('addMovie');
+})
 
 app.listen(port ,function(err){
     err ? console.log(err) : console.log('server is running on port:',port);
