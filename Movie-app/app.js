25fbb2928446969056cf3a61819021614e66c931
Saloni Kahar <salonikahar20@gmail.com>
2025-05-30 01:46:11 +0530

    datashow

--- Movie-app/app.js
+++ Movie-app/app.js
@@ -1,3 +1,5 @@
+const { log } = require('console');
+const exp = require('constants');
 let express =  require('express')
 let port = 8010;
 let app =  express()
@@ -5,14 +7,26 @@ let path = require('path');
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname , 'views'));
+app.use(express.urlencoded())
+
+let data = []
 
 app.get('/', (req , res)=>{
-    return res.render('index');
+    return res.render('index',{
+        'record':data
+    });
 })
 
-app.get('/add', (req , res)=>{
-    return res.render('addMovie');
+app.get('/add', (req, res) => {
+    return res.render('addMovie'); 
+});
+
+app.post('/addMovie' , (req , res)=>{
+    console.log(req.body);
+    data.push(req.body);
+    return res.redirect('/');
 })
+
 
 app.listen(port ,function(err){
     err ? console.log(err) : console.log('server is running on port:',port);
